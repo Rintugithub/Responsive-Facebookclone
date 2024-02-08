ce7f082307a0aea82cd303defeb30d053d2504bd
rintu <rintu-va@industryapps.net>
2024-02-08 14:59:05 +0530

    service worker code added

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'your-app-cache';
+ 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/index.css',
+          '/index.js',
+          '/icon.png'
+        ]);
+      })
+  );
+});
+ 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
